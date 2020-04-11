f993137129ba7cfc31d492c79ae97f3d74a22e99
George Chang <b05902064@ntu.edu.tw>
2020-04-12 00:02:39 +0800

    added prep script.

---
+++ prepare.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ROOT=../
+WANDB=$(pwd)/wandb_inject.txt
+cd $ROOT
+git clone https://github.com/pytorch/fairseq
+cd fairseq
+pip install --user --editable .
+if grep -Fq -f $WANDB fairseq_cli/train.py;then
+    echo "injection already done!"
+else
+    sed -i "/def main\(.*\)\:/r$WANDB" fairseq_cli/train.py
+fi
