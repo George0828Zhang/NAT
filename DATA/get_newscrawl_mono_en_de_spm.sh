80f90ff5d83d3ea70ed04bebc5f43baf0c95a29c
George Chang <b05902064@ntu.edu.tw>
2020-04-30 07:50:50 +0800

    clean procedure

--- DATA/get_newscrawl_mono_en_de_spm.sh
+++ DATA/get_newscrawl_mono_en_de_spm.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-FAIRSEQDIR:=$HOME/Projects/NATLab/fairseq
-ENCODER=$FAIRSEQDIR/examples/roberta/multiprocessing_bpe_encoder.py
 WORKERS=4
-
 BPE_TOKENS=50000
-
+TMP=news.2017.tmp
+OUTDIR=data-bin/news.2017.en-de.spm
+# there will be SPM_MODEL.model and SPM_MODEL.vocab 
+SPM_MODEL=data-bin/news.2017.spm
 
 URLS=(
     "http://data.statmt.org/wmt18/translation-task/news.2017.de.shuffled.deduped.gz"
@@ -40,8 +40,7 @@ for ((i=0;i<${#URLS[@]};++i)); do
     fi
 done
 
-
-TMP=news.2017.tmp
+# train-valid split
 mkdir -p $TMP
 for l in en de; do \
     SOURCE=news.2017.raw/news.2017.${l}.shuffled.deduped
@@ -55,23 +54,22 @@ for l in en de; do \
     if [ -f $TRAIN ]; then
         echo "${TRAIN} found, skipping split."
     else
-        awk '{if (NR%3291 < 50)  print $0; }' $SOURCE > $TRAIN
-        #awk '{if (NR%3291 != 50)  print $0; }' $SOURCE > $TRAIN
+        awk '{if (NR%3291 != 51)  print $0; }' $SOURCE > $TRAIN
     fi
 done
 
+# make training data for spm_train
 TRAIN=$TMP/news.2017.bpe.train
-SPM_MODEL=$TMP/news.2017
-
 if [ -f $TRAIN ]; then
     echo "${TRAIN} found, skipping concat."
 else
     for l in en de; do \
-        # cat news.2017.raw/news.2017.${l}.shuffled.deduped >> $TRAIN
+        # sample this many sentence from each lang to train spm
         shuf -r -n 10000000 news.2017.raw/news.2017.${l}.shuffled.deduped >> $TRAIN
     done
 fi
 
+# spm_train
 echo "spm_train on ${TRAIN}..."
 if [ -f $SPM_MODEL.model ]; then
     echo "${SPM_MODEL}.model found, skipping learn."
@@ -83,10 +81,13 @@ else
     --model_type=bpe
     #--input_sentence_size=1000000
 fi
+
+# spm_encode
+mkdir -p $TMP/news.2017.bpe
 for SPLIT in train valid; do \
     for l in en de; do \
         src=$TMP/news.2017.${SPLIT}.${l}
-        bpe=$TMP/news.2017.${SPLIT}.${l}.bpe
+        bpe=$TMP/news.2017.bpe/${SPLIT}.${l}
         echo "spm_encode to ${src}..."
         if [ -f $bpe ]; then
             echo "$bpe found, skipping apply."
@@ -98,25 +99,20 @@ for SPLIT in train valid; do \
     done
 done
 
-mkdir -p $TMP/news.2017.bpe
-for SPLIT in train valid; do \
-    for l in en de; do \
-        echo "moved to ${TMP}/news.2017.bpe/${SPLIT}.${l}..."
-        mv $TMP/news.2017.${SPLIT}.${l}.bpe $TMP/news.2017.bpe/${SPLIT}.${l}
-    done
-done
-
-TEXT=$TMP/news.2017.bpe
-fairseq-preprocess --source-lang de --target-lang en \
-    --joined-dictionary \
-    --trainpref $TEXT/train \
-    --validpref $TEXT/valid \
-    --destdir data-bin/news.2017.en-de.spm \
-    --workers $WORKERS
-
-cp -r $SPM_MODEL.model data-bin/news.2017.en-de.spm/news.2017.model
-cp -r $SPM_MODEL.vocab data-bin/news.2017.en-de.spm/news.2017.vocab
-cd data-bin/news.2017.en-de.spm
+# binarize
+if [ -d $OUTDIR ]; then
+    echo "$OUTDIR found, skipping preprocess."
+else
+    TEXT=$TMP/news.2017.bpe
+    fairseq-preprocess --source-lang de --target-lang en \
+        --joined-dictionary \
+        --trainpref $TEXT/train \
+        --validpref $TEXT/valid \
+        --destdir $OUTDIR \
+        --workers $WORKERS
+fi
+# format for multilingual denoising.
+cd $OUTDIR
 for l in en de; do \
     mkdir -p ${l}
     for SPLIT in train valid; do \
