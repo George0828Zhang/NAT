b176efdd7bb0fd7dde7618f48ba725d23e477fb9
George0828Zhang <george0828zhang@gmail.com>
2020-06-13 04:43:25 +0800

    updated __init__.py

--- LEANAT/train_leanat_base.sh
+++ LEANAT/train_leanat_base.sh
@@ -1,64 +1,57 @@
 #!/usr/bin/env bash
-TASK=lea_nat_scratch
-DATA=../DATA/data-bin/iwslt16.lee
-CHECK=../naiveNAT/checkpoints/teacher/checkpoint_best.pt
+PATH=$HOME/.local/bin:$PATH
+TASK=leanat
+DATA=../DATA/data-bin/wmt14.accented
+CHECK=../naiveNAT/checkpoints/teacher_wmt14/checkpoint_best.pt
 
 mkdir -p checkpoints/$TASK
 mkdir -p logdir/$TASK
-    
-fairseq-train --task translation_lev_bleu --user-dir . \
-    --noise random_mask \
-    -s en -t de \
-    --load-encoder-only \
+    # --control-vae --control-vae-args '{"v_kl": 3.0, "Kp": 0.01, "Ki": 0.0001, "beta_min": 0.0, "beta_max": 1.0 }' \
+    # --restore-file $CHECK \
+    # --reset-optimizer --reset-dataloader --reset-meters \
+    # --load-weight-level encoder_decoder \    
+    # --use-mask-token \
+
+fairseq-train --task translation_lev_bleu --user-dir .. \
+    --noise no_noise \
+    -s de -t en \
     --restore-file $CHECK \
     --reset-optimizer --reset-dataloader --reset-meters \
+    --load-weight-level encoder \
     --max-tokens 4096 \
-    --max-tokens-valid 2048 \
-    --update-freq 12 \
-    --arch lea_iwslt_16 \
+    --max-tokens-valid 1024 \
+    --empty-cache-freq 1 \
+    --update-freq 25 \
+    --arch lea_nat \
     --share-all-embeddings \
     --apply-bert-init \
-    --lea-loss-factor 1.0 \
-    --lea-attention-heads 2 \
-    --lea-use-embed \
-    --sg-lea-pred \
-    --sg-length-pred \
     --pred-length-offset \
-    --length-loss-factor 0.1 \
+    --length-loss-factor 1.0 \
     --criterion nat_loss \
-    --label-smoothing 0.0 \
+    --label-smoothing 0.1 \
     --optimizer adam \
     --adam-betas '(0.9, 0.98)' \
     --adam-eps 1e-9 \
     --lr-scheduler inverse_sqrt \
-    --warmup-updates 746 \
-    --lr 5e-4 \
+    --warmup-updates 10000 \
+    --lr 2.5e-4 \
     --clip-norm 0.0 \
     --dropout 0.1 \
     --weight-decay 0.0001 \
     --no-epoch-checkpoints \
     --eval-bleu \
-    --eval-bleu-args '{"beam": 1, "max_len_a": 1.2, "max_len_b": 10}' \
+    --eval-bleu-args '{"iter_decode_max_iter": 1}' \
     --eval-bleu-detok moses \
     --eval-bleu-remove-bpe \
+    --eval-bleu-print-samples \
     --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
     --save-dir checkpoints/$TASK \
     --tensorboard-logdir logdir/$TASK \
-    --num-workers 4 \
-    --max-update 100000 \
-    --fp16 \
-    --log-format simple --log-interval 1 \
-    $DATA    
-
-
-    # --eval-bleu-detok space \
-    # --eval-bleu-remove-bpe sentencepiece \
-    # --eval-bleu-print-samples \
-    # --save-interval-updates 500 --keep-interval-updates 5 \
-    # --skip-invalid-size-inputs-valid-test \
-    # --fp16 \
-
-
-
-
-
+    --num-workers 8 \
+    --memory-efficient-fp16 \
+    --fp16-scale-tolerance 3 \
+    --min-loss-scale 1e-8 \
+    --max-update 50000 \
+    --save-interval-updates 500 --keep-interval-updates 1 \
+    --log-format tqdm --log-interval 1 \
+    $DATA
--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
+from .LaNMT import * # put model dir here
 from .LEANAT import * # put model dir here
 from .tasks import *
+from .cmlm_mask import *
